3dbf93a853c72f9d4fdd77d64925a8d4aeba69b7
Dakshina <me@dakshinasd.com>
2015-02-16 10:58:37 +0530

    New modification
    New file modifications

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,15 @@
+$(document).ready(function () {
+    $('.mobile-menu-trigger').click(function(){
+        $(".mobile-menu-content").slideDown();
+    });
+    
+    $(this).click(function () {
+        $(".mobile-menu-content").hide();
+    });
 
+    /* Clicks within the dropdown won't make
+     it past the dropdown itself */
+    $(".mobile-menu-content, .mobile-menu-trigger").click(function (e) {
+        e.stopPropagation();
+    });
+});
